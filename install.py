cc573607e8a24986c7d15abdc0b842eb0435d845
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-03-23 22:18:07 +0800

    Auto install script for llama-cpp-python

---
+++ install.py
@@ -0,0 +1,33 @@
+import launch
+import pkg_resources
+
+
+def get_installed_version(package: str):
+    try:
+        return pkg_resources.get_distribution(package).version
+    except Exception:
+        return None
+
+
+llama_cpp_python_wheel = (
+    "llama-cpp-python --prefer-binary "
+    "--extra-index-url=https://jllllll.github.io/llama-cpp-python-cuBLAS-wheels/{}/{}"
+)
+
+def install_llama_cpp():
+    if get_installed_version("llama_cpp_python") is not None:
+        return
+    print("Attempting to install LLaMA-CPP-Python")
+    import torch
+
+    has_cuda = torch.cuda.is_available()
+    cuda_version = torch.version.cuda.replace(".", "")
+    package = llama_cpp_python_wheel.format("AVX2", f"cu{cuda_version}" if has_cuda else "cpu")
+
+    launch.run_pip(
+        f"install {package}",
+        f"LLaMA-CPP-Python for DanTagGen",
+    )
+
+
+install_llama_cpp()
